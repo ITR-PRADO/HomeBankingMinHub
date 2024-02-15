80acb840309c056313f92e0f0251ffce5464d67b
Ignacio_Prado <Ignacio_Prado@ITRP1106>
2024-02-15 14:26:44 -0300

    Add project files.

---
+++ HomeBankingMinHub/Models/DBInitializer.cs
@@ -0,0 +1,27 @@
+﻿namespace HomeBankingMinHub.Models
+{
+    public class DBInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client { Email = "vcoronado@gmail.com", FirstName="Victor", LastName="Coronado", Password="111111"},
+                    new Client { Email = "jperez@gmail.com", FirstName="Juan", LastName="Perez", Password="222222"},
+                    new Client { Email = "ppicapiedra@gmail.com", FirstName="Pedro", LastName="Picapiedra", Password="333333"},
+                    new Client { Email = "pcalvo@gmail.com", FirstName="Pablo", LastName="Calvo", Password="444444"},
+                    new Client { Email = "iprado@gmail.com", FirstName="Ignacio", LastName="Prado", Password="123456"}
+
+                };
+
+                context.Clients.AddRange(clients);
+
+                //guardamos
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
